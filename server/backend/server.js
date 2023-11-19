783cb4a4c16c1c7c57f5934dd8f648daf110eafd
mhk-temp <isa000@kaist.ac.kr>
2023-11-19 23:48:24 +0900

    Basic Frontend Backend Implement

--- server/backend/server.js
+++ server/backend/server.js
@@ -1,66 +1,74 @@
-// Import necessary modules
-const express = require('express');
-const http = require('http');
-const mysql = require('mysql');
+const express = require("express");
+const path = require("path");
 const cors = require("cors");
-const { Server } = require('socket.io');
+//const https = require("https");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const { writeFile, copyFileSync } = require("fs");
 
-// Create an Express application
+const { createConnection } = require("mysql");
+
+require("dotenv").config();
+
 const app = express();
 
-app.use(express.json());
-app.use(
-  cors({
-    origin: true
-  })
+const basePath = path.join(__dirname, "dist");
+const frontPath = path.normalize(
+  path.join(__dirname, "..", "frontend", "src", "assets")
+);
+app.use(express.static(basePath));
+app.use(cors());
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+const port = process.env.port || 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(basePath, "index.html"));
+});
+
+httpServer.listen(port, () =>
+  console.log(`server listening at http://localhost:${port}`)
 );
 
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors:{
-    origin: true
-  }
+/* Code for backend database
+const connection = createConnection({
+  host: process.env.host || "localhost",
+  user: process.env.user || "root",
+  password: process.env.password || "root",
+  database: process.env.database || "infos",
 });
 
-const dbConnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'your_database_name'
-});
+connection.connect();
+*/
 
-dbConnection.connect();
-
-// WebSocket connection
-io.on('connection', (socket) => {
-  console.log('Client connected');
-
-  socket.on('deviceData', (data) => {
-    console.log(data);
-    const deviceData = data;
-    const msg = deviceData.deviceData;
-
-    const query = 'INSERT INTO your_table_name (data) VALUES (?)';
-    dbConnection.query(query, [msg], (err, result) => {
-      if (err) {
-        console.error('Error inserting device data:', err);
-        return;
-      }
-      console.log("Inserting device data success.");
-      return;
+io.on("connection", (socket) => {
+  /* Code for backend database
+  socket.on("device backend", (name, file) => {
+    console.log(`receive file ${name} from device`);
+    writeFile(path.join(__dirname, "dist", "assets", name), file, (err) => {
+      callback({ message: err ? "write failure" : "write success" });
     });
-
-    // Notify connected clients about the new data
-    io.emit('newData', msg);
-
-    console.log('Received data from client:', msg);
+    const query = `INSERT INTO ${process.env.table || "images"} VALUES ${name}`;
+    connection.query(query, (err, rows, field) => {
+      callback({ message: err ? "insert failure" : "insert success" });
+    });
   });
-  // Handle disconnection
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
+  */
+  socket.on("frontend backend", (command) => {
+    console.log(command);
+    const time = "screen"; // Date.now().toString();
+    /* Code for backend frontend
+    console.log(path.join(frontPath, "screen.jpg"));
+    console.log(path.join(frontPath, time + ".jpg"));
+    copyFileSync(
+      path.join(frontPath, "screen.jpg"),
+      path.join(frontPath, time + ".jpg")
+    );
+    */
+    socket.emit("backend frontend", { time: time });
   });
-});
-
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
 });
